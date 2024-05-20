b86fbae0836c7dd4700bf41299f3a37827301451
rieerep <114030611+rieerep@users.noreply.github.com>
2024-05-20 14:24:08 +0200

    added about page and route to it

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './styles/App.css';
 import CardList from './components/CardList.jsx';
 import Footer from './components/Footer.jsx';
 import MyPage from './components/MyPage.jsx';
+import About from './components/About.jsx';
 import { Link, Routes, Route } from 'react-router-dom';
 
 
@@ -12,18 +13,21 @@ function App() {
     <>
       <body className='body'>
         <div className='banner'>
-          <h1>Board Game Library</h1>
+          <h1>The Board Game Library</h1>
         </div>
         <nav className='nav-bar'>
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/mypage">My Page</Link></li>
+            <li><Link to="/about">About</Link></li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<CardList />}>
           </Route>
           <Route path="/MyPage" element={<MyPage />}>
+          </Route>
+          <Route path="/About" element={<About />}>
           </Route>
         </Routes>
 
---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div className="about">
+            <h2>About</h2>
+            <p>Welcome to the Board Game Library! This is a collection of board games that you can browse and add to your collection. You can also view your collection and remove games from it. </p>
+        </div>
+    );
+}
+
+export default About;
